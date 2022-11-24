6d7d6019597d3f50b746af58bd0b75c7013d8cef
muhammedcakar <70714211+muhammedcakar@users.noreply.github.com>
2022-11-24 19:12:58 +0300

    Add files via upload

---
+++ form.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Veri Tabanı Kayıt</title>
+  <style>
+  table, td, th {
+  border: 1px solid;
+  }  
+
+   table {
+  width: 100%;
+  border-collapse: collapse;
+}
+</style>  
+</head>
+
+<body style="text-align:center">
+   
+           <form method="post" action="control.php">
+            <b><label for="fname">Adınız Soyadınız:</label></b><br><br>
+            <input type="text" name="fnamesurname" required  ><br><br>
+            <b><label for="lname">TC Kimlik No:</label></b><br><br>
+            <input type="text"  name="tckimlik"  ><br><br>
+           <input type="submit" name ="save" value="Bilgileri Kaydet"><br>
+           </form><br><br>
+           <?php
+            include ("list.php"); 
+  //-----------------------------------------------------------
+
+?>
+
+</body>
+
+</html>
