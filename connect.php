b36187cc4eed3a2017ebfc63f8db74dfa63780d1
muhammedcakar <70714211+muhammedcakar@users.noreply.github.com>
2022-11-24 19:13:43 +0300

    Delete connect.php

--- connect.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-    $connect = new PDO("mysql:host=localhost;dbname=kayitlardb", "root", "");
-    $connect->exec("SET NAMES utf8");
-    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-?>
